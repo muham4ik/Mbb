d94a761f9e4538e230402690183c01c83fee3638
muham4ik <muhammadjonmajidov0@gmail.com>
2024-08-16 17:58:15 +0500

    Ended

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav');
+let menuItem = document.querySelectorAll('.nav__link');
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
+
+menuItem.forEach (function(menuItem) {
+  menuItem.addEventListener('click',function(){
+          menuBtn.classList.toggle('active');
+          menu.classList.toggle('active');
+  })
+})
+
+
+
+
+
+
+
+
+
+
 const burger = document.querySelector('.burger');
 const navbarList = document.querySelector('.navbar_list');
 
@@ -5,3 +32,6 @@ burger.addEventListener('click', () => {
   navbarList.classList.toggle('nav-active');
   burger.classList.toggle('toggle');
 });
+
+
+
